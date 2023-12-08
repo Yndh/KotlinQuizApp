aa78466da4c809748b492e807e27d68431cd7be7
Adrian <114053643+AdrianPrzybylskiTIE9@users.noreply.github.com>
2023-12-08 12:30:02 +0100

    Display Question

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -1,4 +1,33 @@
 package com.example.quizapp
 
 object Constants {
+    fun getQuestions(): ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+
+        val questionOne = Question(
+            1, "Jakie jest przeciętne życie kota domowego?", listOf(
+                "Około 5 lat",
+                "Około 10 lat",
+                "Około 15 lat",
+                "Około 20 lat"
+            ), 2, 30
+        )
+
+        val questionTwo = Question(
+            2, "Najglupszy kot to", listOf(
+                "Pchlarz",
+                "Milly",
+                "Jinx",
+                "Teo"
+            ), 3, 30
+        )
+
+
+        questionsList.add(questionOne)
+        questionsList.add(questionTwo)
+
+        return questionsList
+    }
+
+
 }
--- app/src/main/java/com/example/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -1,11 +1,61 @@
 package com.example.quizapp
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.marginBottom
 
 class QuizActivity : AppCompatActivity() {
+    private lateinit var questionTextView: TextView
+    private lateinit var questionIndexTextView: TextView
+    private lateinit var layout: LinearLayout
+    private lateinit var questions: List<Question>
+    private var currQuestion = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        questionTextView = findViewById(R.id.questionTextView)
+        questionIndexTextView = findViewById(R.id.questionIndexTextView)
+        layout = findViewById(R.id.answersLayout)
+
+        questions = Constants.getQuestions()
+
+        showQuestion()
+    }
+
+    private fun showQuestion() {
+        layout.removeAllViews()
+
+        val currentQuestion = questions[currQuestion]
+        questionTextView.text = currentQuestion.question
+        questionIndexTextView.text = "Pytanie ${currQuestion+1}"
+
+        currentQuestion.answers.forEach { answer ->
+            val button = Button(this)
+            button.text = answer
+            val layoutParams = LinearLayout.LayoutParams(
+                650,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(0, 0, 0, 20)
+            button.layoutParams = layoutParams
+            button.setBackgroundResource(R.drawable.border)
+            button.setTextColor(resources.getColor(R.color.secondaryFontColor))
+            button.setPadding(70, 15, 70, 20)
+            button.textSize = 18f
+            val archivio : Typeface? = ResourcesCompat.getFont(this, R.font.archivo_black)
+            button.typeface = archivio
+            button.setOnClickListener {
+                // Check Question
+            }
+            layout.addView(button)
+        }
+
     }
 }
