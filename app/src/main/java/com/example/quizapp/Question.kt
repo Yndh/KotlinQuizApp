c67f9548e710a822c2ad953604cb775c948b9834
Adrian <114053643+AdrianPrzybylskiTIE9@users.noreply.github.com>
2023-12-08 11:58:14 +0100

    Update UI

--- app/src/main/java/com/example/quizapp/Question.kt
+++ app/src/main/java/com/example/quizapp/Question.kt
@@ -3,7 +3,6 @@ package com.example.quizapp
 data class Question(
     val id: Int,
     val question: String,
-    val image: Int,
     val answers: List<String>,
     val answer: Int,
     val answerTime: Long,
