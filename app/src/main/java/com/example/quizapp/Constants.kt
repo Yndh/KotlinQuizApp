5aa71315f14346646c5193c6ffed5710b5280116
Adrian <114053643+AdrianPrzybylskiTIE9@users.noreply.github.com>
2023-12-14 13:25:07 +0100

    Add checkAnswer function

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -4,30 +4,61 @@ object Constants {
     fun getQuestions(): ArrayList<Question>{
         val questionsList = ArrayList<Question>()
 
-        val questionOne = Question(
-            1, "Jakie jest przeciętne życie kota domowego?", listOf(
-                "Około 5 lat",
-                "Około 10 lat",
-                "Około 15 lat",
-                "Około 20 lat"
-            ), 2, 30
+        val questionsData = listOf(
+            Question(
+                1, "Wybierz poprawny opis Kurvinoxa", listOf(
+                    "Silny ale wolny",
+                    "Sprytny ale głupi",
+                    "Mądry ale słaby",
+                    "Mądry i silny"
+                ), 2, 30
+            ),
+            Question(
+                2, "Jak Mkbewe jest nazywany przez kosmitów", listOf(
+                    "Szatan",
+                    "Zabójca",
+                    "Diabeł",
+                    "Behemot"
+                ), 3, 30
+            ),
+            Question(
+                3, "Kto ma władzę w galaktyce Kujwdubie?", listOf(
+                    "Papież",
+                    "Sułtan Kosmitów",
+                    "Senator Chujewów",
+                    "Gwiezdna Flota"
+                ), 1, 30
+            ),
+            Question(
+                4, "Ile procent alkoholu ma wódka \"Skurwolańska\"", listOf(
+                    "9%",
+                    "7%",
+                    "37.5%",
+                    "40%"
+                ), 2, 30
+            ),
+            Question(
+                5, "Jak ma na imię Kapitan Bomba", listOf(
+                    "Sebastian",
+                    "Tytus",
+                    "Janek",
+                    "Grzesiek"
+                ), 2, 30
+            ),
+            Question(
+                6, "Co obiecał za darmo dla wszystkich Domino Jachaś", listOf(
+                    "Hot Dogi",
+                    "Burgery",
+                    "Pizzę Hawajską",
+                    "Kebaba z Chrzanem"
+                ), 3, 30
+            )
         )
 
-        val questionTwo = Question(
-            2, "Najglupszy kot to", listOf(
-                "Pchlarz",
-                "Milly",
-                "Jinx",
-                "Teo"
-            ), 3, 30
-        )
-
-
-        questionsList.add(questionOne)
-        questionsList.add(questionTwo)
+        for (data in questionsData) {
+            questionsList.add(data)
+        }
 
         return questionsList
     }
-
-
 }
--- app/src/main/java/com/example/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quizapp
 
+import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,8 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
+import android.os.Handler
+import android.widget.ProgressBar
 import androidx.core.view.marginBottom
 
 class QuizActivity : AppCompatActivity() {
@@ -14,7 +17,9 @@ class QuizActivity : AppCompatActivity() {
     private lateinit var questionIndexTextView: TextView
     private lateinit var layout: LinearLayout
     private lateinit var questions: List<Question>
+    private lateinit var progressBar: ProgressBar
     private var currQuestion = 0
+    private var isAnswerChecked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +27,11 @@ class QuizActivity : AppCompatActivity() {
 
         questionTextView = findViewById(R.id.questionTextView)
         questionIndexTextView = findViewById(R.id.questionIndexTextView)
+        progressBar = findViewById(R.id.quizProgressBar)
         layout = findViewById(R.id.answersLayout)
 
         questions = Constants.getQuestions()
+        progressBar.max = questions.size
 
         showQuestion()
     }
@@ -34,28 +41,49 @@ class QuizActivity : AppCompatActivity() {
 
         val currentQuestion = questions[currQuestion]
         questionTextView.text = currentQuestion.question
-        questionIndexTextView.text = "Pytanie ${currQuestion+1}"
+        questionIndexTextView.text = "Pytanie ${currQuestion + 1}"
+        progressBar.progress = currQuestion+1
 
-        currentQuestion.answers.forEach { answer ->
+        currentQuestion.answers.forEachIndexed { index, answer ->
             val button = Button(this)
             button.text = answer
             val layoutParams = LinearLayout.LayoutParams(
-                650,
+                750,
                 LinearLayout.LayoutParams.WRAP_CONTENT
             )
-            layoutParams.setMargins(0, 0, 0, 20)
+            layoutParams.setMargins(0, 0, 0, 50)
             button.layoutParams = layoutParams
             button.setBackgroundResource(R.drawable.border)
             button.setTextColor(resources.getColor(R.color.secondaryFontColor))
-            button.setPadding(70, 15, 70, 20)
-            button.textSize = 18f
-            val archivio : Typeface? = ResourcesCompat.getFont(this, R.font.archivo_black)
+            button.setPadding(70, 50, 70, 55)
+            button.textSize = 20f
+            val archivio: Typeface? = ResourcesCompat.getFont(this, R.font.archivo_black)
             button.typeface = archivio
             button.setOnClickListener {
-                // Check Question
+                if (!isAnswerChecked) {
+                    isAnswerChecked = true
+                    checkAnswer(index+1, button)
+                }
             }
             layout.addView(button)
         }
+    }
 
+    private fun checkAnswer(selectedAnswer: Int, selectedButton: Button) {
+        val currentQuestion = questions[currQuestion]
+        val correctAnswer = currentQuestion.answer
+
+        selectedButton.setBackgroundResource( if (selectedAnswer == correctAnswer) R.drawable.border_green else R.drawable.border_red)
+
+        val handler = Handler()
+        handler.postDelayed({
+            currQuestion++
+            if (currQuestion < questions.size) {
+                isAnswerChecked = false
+                showQuestion()
+            } else {
+                // koniec quizu
+            }
+        }, 3000)
     }
 }
