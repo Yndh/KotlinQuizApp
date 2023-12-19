e0db11816e6d11bc8a9aefccbf62559b0f85a21b
Adrian <114053643+AdrianPrzybylskiTIE9@users.noreply.github.com>
2023-12-19 14:01:46 +0100

    Add Quiz Answer Timer

--- app/src/main/java/com/example/quizapp/QuizActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -1,17 +1,16 @@
 package com.example.quizapp
 
 import android.content.Intent
-import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import android.os.Handler
 import android.widget.ProgressBar
-import androidx.core.view.marginBottom
 
 class QuizActivity : AppCompatActivity() {
     private lateinit var questionTextView: TextView
@@ -19,11 +18,13 @@ class QuizActivity : AppCompatActivity() {
     private lateinit var layout: LinearLayout
     private lateinit var questions: List<Question>
     private lateinit var progressBar: ProgressBar
+    private lateinit var answerTimeProgressbar: ProgressBar
     private var currQuestion = 0
     private var isAnswerChecked = false
     private var correctAnswers = 0
     private var wrongAnswers = 0
-
+    private lateinit var countDownTimer: CountDownTimer
+    private val timeInterval: Long = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
@@ -31,6 +32,7 @@ class QuizActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.questionTextView)
         questionIndexTextView = findViewById(R.id.questionIndexTextView)
         progressBar = findViewById(R.id.quizProgressBar)
+        answerTimeProgressbar = findViewById(R.id.answerTimeProgressBar)
         layout = findViewById(R.id.answersLayout)
 
         questions = Constants.getQuestions()
@@ -46,8 +48,35 @@ class QuizActivity : AppCompatActivity() {
         questionTextView.text = currentQuestion.question
         questionIndexTextView.text = "Pytanie ${currQuestion + 1}"
         progressBar.progress = currQuestion + 1
+        answerTimeProgressbar.max = currentQuestion.answerTime.toInt()
 
         val shuffledAnswers = currentQuestion.answers.shuffled()
+
+        var isTimeUp = false
+
+        countDownTimer = object : CountDownTimer(currentQuestion.answerTime * 1000L, timeInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val progress = (millisUntilFinished / 1000).toInt()
+                answerTimeProgressbar.progress = progress
+            }
+
+            override fun onFinish() {
+                isTimeUp = true
+                wrongAnswers++
+                currQuestion++
+                if (currQuestion < questions.size) {
+                    showQuestion()
+                } else {
+                    val intent = Intent(this@QuizActivity, QuizzResult::class.java)
+                    intent.putExtra("CORRECT_ANSWERS", correctAnswers)
+                    intent.putExtra("WRONG_ANSWERS", wrongAnswers)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+
+        countDownTimer.start()
 
         shuffledAnswers.forEachIndexed { index, answer ->
             val originalIndex = currentQuestion.answers.indexOf(answer)
@@ -66,15 +95,15 @@ class QuizActivity : AppCompatActivity() {
             val archivio: Typeface? = ResourcesCompat.getFont(this, R.font.archivo_black)
             button.typeface = archivio
             button.setOnClickListener {
-                if (!isAnswerChecked) {
+                if (!isAnswerChecked && !isTimeUp) {
                     isAnswerChecked = true
+                    countDownTimer.cancel()
                     checkAnswer(originalIndex + 1, button)
                 }
             }
             layout.addView(button)
         }
     }
-
 
     private fun checkAnswer(selectedAnswer: Int, selectedButton: Button) {
         val currentQuestion = questions[currQuestion]
@@ -87,6 +116,8 @@ class QuizActivity : AppCompatActivity() {
             selectedButton.setBackgroundResource(R.drawable.border_red)
             wrongAnswers++
         }
+
+        isAnswerChecked = true
 
         val handler = Handler()
         handler.postDelayed({
